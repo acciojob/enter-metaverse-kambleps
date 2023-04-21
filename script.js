eb1709f7117092d70755103e23b5887f03fddb29
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-function onbutton{
-	document.querySelector(#status).innerHTML = "<h1>Entered Metaverse</h1>";
+function chnageText(){
+    document.querySelector("#status").innerHTML = "<h1>Entered Metaverse</h1>";
 }
